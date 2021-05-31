3be60f43ed4d90b8674465f7da1c481c1f96cc20
do02reen24 <do02reen24@gmail.com>
2021-06-01 00:50:45 +0900

    feat : server typescript 적용
    - server에 typescript를 적용함
    - index와 app으로 분리함

---
+++ server/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+class App {
+  public application: express.Application;
+
+  constructor() {
+    this.application = express();
+    this.router();
+  }
+
+  private router(): void {
+    this.application.get('/', (req: express.Request, res: express.Response) => {
+      res.send('Hello World!');
+    });
+  }
+}
+
+export default App;
---
+++ server/index.ts
@@ -0,0 +1,5 @@
+import App from './app';
+
+const app = new App().application;
+
+app.listen(8000, () => console.log('Server listening on port 8000'));
