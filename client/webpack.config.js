748bb23728611716de289b8169307c570c00f71a
do02reen24 <do02reen24@gmail.com>
2021-06-03 14:53:00 +0900

    feat : client 프로젝트 생성

---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: './src/index',
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      // index.html에 output에서 만들어진 bundle.js를 적용하여, dist에 새로운 html 파일 생성
+      template: `./public/index.html`,
+    }),
+  ],
+};
