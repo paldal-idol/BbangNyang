2b905bb0a15b85f7dcdcba7c0754357d03234c21
SuhJaemyoung <kevindec18@ajou.ac.kr>
2021-06-19 16:14:12 +0900

    feat : 룰북 페이지 추가

---
+++ client/src/pages/RuleBookPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import RuleBook from '@organisms/RuleBook';
+
+const RuleBookPage: React.FC = () => {
+  return <>{/* <RuleBook /> */}</>;
+};
+
+export default RuleBookPage;
