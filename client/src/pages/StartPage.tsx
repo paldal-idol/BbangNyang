a297df2269676053418e65cf921ca70e85d157f8
coreight98 <maxcha98@naver.com>
2021-06-26 08:34:26 +0900

    style change dice position

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import BbangNyangTitle from '../components/organisms/BbangNyangTitle';
 import Bakery from '../components/organisms/Bakery';
-import Dice from '../components/organisms/Dice';
 
 const StartPage: React.FC = () => {
   return (
     <>
       <BbangNyangTitle />
       <Bakery />
-      <Dice />
     </>
   );
 };
