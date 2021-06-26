1578f0a73dc532d9ef6bea9d26b6cb20b57d1536
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-06-26 19:38:56 +0900

    feat : 공백 입력 무시, style 추가

--- client/src/components/molecules/WaitingRoomChat.tsx
+++ client/src/components/molecules/WaitingRoomChat.tsx
@@ -1,5 +1,24 @@
 import * as React from 'react';
-
+import styled from 'styled-components';
+import RoundSquareButton from '@atoms/RoundSquareButton';
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  padding: 5px;
+`;
+const StyledInput = styled.input`
+  flex-grow: 1;
+  border: 0px;
+  margin-right: 1px;
+`;
+const StyledButton = styled.button`
+  background-color: white;
+  border: 1px solid gray;
+  padding: 8px 12px;
+  radius: 8px;
+  margin-left: 1px;
+`;
 const WaitingRoomChat = () => {
   const [chat, setChat] = React.useState('');
   const [chatList, setChatList] = React.useState([]);
@@ -9,10 +28,12 @@ const WaitingRoomChat = () => {
   };
 
   const onSubmit = () => {
-    const newChatList = chatList.concat();
-    newChatList.push(chat);
-    setChatList((current) => newChatList);
-    setChat((current) => '');
+    if (chat !== '') {
+      const newChatList = chatList.concat();
+      newChatList.push(chat);
+      setChatList((current) => newChatList);
+      setChat((current) => '');
+    }
   };
 
   //할 일 : const sendChat = () => {};
@@ -25,8 +46,12 @@ const WaitingRoomChat = () => {
             return <li className="chat-message">username : {chat}</li>;
           })}
         </ul>
-        <input type="text" name="chatting" value={chat} onChange={onTyping} />
-        <button onClick={onSubmit}>전송</button>
+        <Container>
+          <StyledInput type="text" name="chatting" value={chat} onChange={onTyping} />
+          <RoundSquareButton size="sm" variant="gray" onClick={onSubmit}>
+            전송
+          </RoundSquareButton>
+        </Container>
       </div>
     </div>
   );
