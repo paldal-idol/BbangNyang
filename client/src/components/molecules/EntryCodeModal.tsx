669741954e74ad5aaa6b3c20dde19fa6c60e805b
do02reen24 <do02reen24@gmail.com>
2021-07-03 23:39:37 +0900

    feat : EntryCodeModal 디자인 및 링크 연결

--- client/src/components/molecules/EntryCodeModal.tsx
+++ client/src/components/molecules/EntryCodeModal.tsx
@@ -1,30 +1,54 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
 import modalState from '@store/modal';
 import Modal from '@atoms/Modal';
+import color from '@theme/color';
+
+interface buttonProps {
+  backgroundColor: string;
+}
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 600px;
-  height: 200px;
-  background-color: white;
+  width: 320px;
+  height: 240px;
+  background-color: ${color.primary.darkYellow};
   border-radius: 10px;
 `;
 
-const Text = styled.p``;
+const CodeInput = styled.input`
+  width: 220px;
+  height: 45px;
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 16px;
+  border: 0px solid black;
+`;
 
-const CodeInput = styled.input``;
-
-const EntryButton = styled.button``;
-
-const CloseButton = styled.button``;
+const Button = styled.button<buttonProps>`
+  width: 230px;
+  height: 35px;
+  font-size: 18px;
+  color: white;
+  background-color: ${(props) => props.backgroundColor};
+  cursor: pointer;
+  border: 0px;
+`;
 
 const EntryCodeModal: React.FC = () => {
+  const history = useHistory();
   const setModal = useSetRecoilState(modalState);
+
+  const codeHandler = () => {
+    // TODO : 올바른 입장 코드인지 확인하는 코드 작성
+    closeModal();
+    history.push('/waiting');
+  };
 
   const closeModal = () => {
     setModal('');
@@ -33,10 +57,13 @@ const EntryCodeModal: React.FC = () => {
   return (
     <Modal>
       <Content>
-        <CloseButton onClick={closeModal}>X</CloseButton>
-        <Text>방 코드를 입력해주세요.</Text>
-        <CodeInput />
-        <EntryButton>입장</EntryButton>
+        <CodeInput placeholder="코드를 입력해주세요." />
+        <Button backgroundColor={color.button.orange} onClick={codeHandler}>
+          입장
+        </Button>
+        <Button backgroundColor={color.button.darkGray} onClick={closeModal}>
+          돌아가기
+        </Button>
       </Content>
     </Modal>
   );
