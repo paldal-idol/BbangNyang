ac06848e9852188017b57a636d1cae19d86671f1
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-07-05 01:09:44 +0900

    feat : backend에서 User 목록 가져오기 추가

--- client/src/components/molecules/WaitingRoomUsers.tsx
+++ client/src/components/molecules/WaitingRoomUsers.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import CircleButton from '@atoms/CircleButton';
 
+const Container = styled.div``;
 const WaitingRoomUsers = () => {
-  const Container = styled.div``;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/waiting')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers((current) => data);
+      });
+    return;
+  }, []);
+
   return (
     <Container>
       <h2>참가자</h2>
@@ -32,6 +44,12 @@ const WaitingRoomUsers = () => {
             x
           </CircleButton>
         </li>
+      </ul>
+      <h2>from server</h2>
+      <ul>
+        {users.map((user) => {
+          return <li key={user.id}>{user.name}</li>;
+        })}
       </ul>
     </Container>
   );
