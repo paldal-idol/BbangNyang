6912a9c87868d7dc4cc5a55314d36e6c07320c21
coreight98 <maxcha98@naver.com>
2021-07-09 19:14:57 +0900

    ffeat: dice_image and show image

--- client/src/components/organisms/Dice_svg.tsx
+++ client/src/components/organisms/Dice_svg.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import dice_1 from '@img/dice/dice_1.PNG';
+import dice_2 from '@img/dice/dice_2.PNG';
+import dice_3 from '@img/dice/dice_3.PNG';
+import dice_4 from '@img/dice/dice_4.PNG';
+import dice_5 from '@img/dice/dice_5.PNG';
+import dice_6 from '@img/dice/dice_6.PNG';
+
+const dice_list = [dice_1, dice_2, dice_3, dice_4, dice_5, dice_6];
 const Dice_svg = styled.svg`
   z-index: 1;
   position: absolute;
 `;
-const Dice_h1 = styled.h1`
+const Dice_img = styled.img`
   margin-top: 200px;
+  margin-left: 100px;
+  z-index: 2;
+  position: absolute;
+  width: 100px;
+`;
+const Dice_h1 = styled.h1`
+  margin-top: 240px;
   margin-left: 140px;
   z-index: 2;
   position: absolute;
@@ -18,68 +33,54 @@ const Dice_Button = styled.button`
   position: absolute;
   width: 100px;
 `;
-class Dice extends React.Component {
-  dice_number;
-  interval;
-  state = {
-    x: 150,
-    y: 50,
-  };
-  mounted: boolean;
+const Dice = () => {
+  const [intervalFunc, setIntervalFunc] = useState(null);
+  const [diceImage, setDiceImage] = useState(dice_list[0]);
+  const [diceNumber, setDiceNumber] = useState(0);
+  const [coordinate, setCoordinate] = useState({ x: 150, y: 50 });
 
-  componentDidMount() {
-    this.mounted = true;
-  }
-  componentWillUnmount() {
-    this.mounted = false;
-  }
-  tick = () => {
+  const tick = () => {
     const time = Date.now() / 500;
-    this.setState({
+    setCoordinate({
       x: 150 + 100 * Math.cos(time),
       y: 150 + 100 * -Math.abs(Math.sin(time)),
     });
-    if (this.mounted) {
-      requestAnimationFrame(this.tick);
-    } else {
-      this.setState({
-        x: 150,
-        y: 50,
-      });
-    }
+    setDiceImage(dice_list[Math.floor(time % 6)]);
+    let d = Math.random() * (coordinate.x + coordinate.y) + coordinate.y;
+    setDiceNumber(Math.floor(((Math.random() * d) % 10) % 6) + 1);
   };
-  down = () => {
-    this.mounted = true;
-    this.interval = setInterval(this.tick, 0);
-  };
-  up = () => {
-    this.mounted = false;
-    clearInterval(this.interval);
-    let d = Math.random() * (this.state.x + this.state.y) + this.state.y;
-    this.dice_number = Math.floor((d % 10) % 6) + 1;
-    console.log(this.dice_number);
-  };
-  render() {
-    let count;
-    const { x, y } = this.state;
-    const mouse_down = this.down;
-    const mouse_up = this.up;
-    return (
-      <>
-        <div>
-          <Dice_svg width="300" height="300">
-            {/* center */}
 
-            <circle r="5" cx="150" cy="150" fill="black" />
-            <circle r="5" cx={x} cy={y} fill="black" />
-          </Dice_svg>
-          <Dice_h1>{this.dice_number}</Dice_h1>
-          <Dice_Button onMouseDown={mouse_down} onMouseUp={mouse_up}>
-            클릭
-          </Dice_Button>
-        </div>
-      </>
-    );
-  }
-}
+  const down = () => {
+    setIntervalFunc(setInterval(tick, 0));
+  };
+
+  const up = () => {
+    clearInterval(intervalFunc);
+    console.log(diceNumber);
+    setDiceImage(dice_list[diceNumber - 1]);
+  };
+
+  useEffect(() => {
+    tick();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <Dice_svg width="300" height="300">
+          {/* center */}
+
+          <circle r="5" cx="150" cy="150" fill="black" />
+          <circle r="5" cx={coordinate.x} cy={coordinate.y} fill="black" />
+        </Dice_svg>
+        <Dice_img src={diceImage} />
+        {/* <Dice_h1>{diceNumber}</Dice_h1> */}
+        <Dice_Button onMouseDown={down} onMouseUp={up}>
+          클릭
+        </Dice_Button>
+      </div>
+    </>
+  );
+};
+
 export default Dice;
