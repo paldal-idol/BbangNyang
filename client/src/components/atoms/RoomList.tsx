c8d1981a47bbf10eeefe1bf1d8dd2c5afd5916f5
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-07-10 18:58:26 +0900

    feat : 채팅 컴포넌트 추가

---
+++ client/src/components/atoms/RoomList.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const RoomList = () => {
+  const [rooms, setRooms] = useState([]);
+
+  const getRooms = async () => {
+    const res = await fetch('http://localhost:8000/');
+    const body = await res.json();
+    setRooms(body);
+  };
+
+  useEffect(() => {
+    getRooms();
+  });
+
+  if (rooms.length === 0) {
+    return (
+      <div>
+        <h2>방 목록</h2>
+        <span>방이 없습니다.</span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>방 목록</h2>
+        <ul>
+          {rooms.map((room) => {
+            return (
+              <li key={room.roomId}>
+                {room.roomId}번 방 - {room.roomState}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+};
+export default RoomList;
