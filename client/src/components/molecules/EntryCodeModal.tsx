e4f724e69afaec807d64df3252fdfea3c830bf1f
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-06 00:34:45 +0900

    feat : get random name when entering room

--- client/src/components/molecules/EntryCodeModal.tsx
+++ client/src/components/molecules/EntryCodeModal.tsx
@@ -57,19 +57,18 @@ const EntryCodeModal: React.FC = () => {
     // TODO : 방장 채팅 오류 -> 소켓 문제인 것 같습니다
     // TODO : 입장시에 닉네임 중복 체크 OR 랜덤 닉네임 생성, 중복되면 채팅 오류
     // setUser('test..');
-    closeModal();
-    history.push(`/waiting`);
+    axios.get('http://localhost:8000/getName').then(res=>{
+      setUser(res.data.name);
+    }).then(()=>{
+      closeModal();
+      history.push(`/waiting`);
+    })
   };
 
   const closeModal = () => {
     setModal('');
   };
-  const getCode = (callback) => {
-    axios.get('http://localhost:8000/makeRoom').then((res) => {
-      callback(res.data);
-    });
-  };
-
+  
   return (
     <Modal>
       <Content>
@@ -77,33 +76,11 @@ const EntryCodeModal: React.FC = () => {
           placeholder="입장코드"
           onChange={(event) => {
             setRoom(event.target.value);
-            setUser('test..');
           }}
         />
-        {/* <CodeInput
-          placeholder="닉네임을 입력해주세요"
-          onChange={(event) => {
-            setUser(event.target.value);
-          }}
-        ></CodeInput> */}
         <Button backgroundColor={color.button.orange} onClick={codeHandler}>
           입장하기
         </Button>
-        {/* <Button
-          backgroundColor={color.button.orange}
-          onClick={() => {
-            if (!user) {
-              alert('닉네임을 입력해주세요');
-            } else {
-              getCode(async (code) => {
-                setRoom(code);
-                codeHandler();
-              });
-            }
-          }}
-        >
-          방만들기
-        </Button> */}
         <Button backgroundColor={color.button.darkGray} onClick={closeModal}>
           돌아가기
         </Button>
