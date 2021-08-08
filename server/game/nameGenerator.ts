9da1039e76c71f9cb285f6e2afdb0f9245aa7ba9
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-08-08 16:38:23 +0900

    feat : check duplicated name

--- server/game/nameGenerator.ts
+++ server/game/nameGenerator.ts
@@ -3,6 +3,7 @@
 Random Name Generator for Javascript
 */
 import { firstName, lastName } from './nameList';
+const {users}=require('../socket/users');
 
 function capFirst(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -12,11 +13,19 @@ function getRandomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function checkDuplicate(name:string){
+  if(!users) return true;
+  const names = users.map((user:any)=>user.name);
+  if(names.find((existName:string)=>existName===name)===-1) return true;
+  else return false;
+}
 function generateName() {
-  let name: string =
-    capFirst(firstName[getRandomInt(0, firstName.length + 1)]) +
-    ' ' +
-    capFirst(lastName[getRandomInt(0, lastName.length + 1)]);
-  return name;
+  while(true){
+    let name: string =
+      capFirst(firstName[getRandomInt(0, firstName.length + 1)]) +
+      ' ' +
+      capFirst(lastName[getRandomInt(0, lastName.length + 1)]);
+    if(checkDuplicate(name)) return name;
+  }
 }
 module.exports = {generateName};
