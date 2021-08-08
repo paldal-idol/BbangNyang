469bca9433078c228c15ee609647b50e697fdba5
kevin <kevin@lsware.com>
2021-08-08 11:34:48 +0900

    feat : 페이지 가이드 기능 추가

--- client/src/components/molecules/StartPageGuide.tsx
+++ client/src/components/molecules/StartPageGuide.tsx
@@ -1,28 +1,74 @@
-import React from 'react';
-import PageGuide from "@atoms/PageGuide";
-import GuideContent from "@atoms/GuideContent";
-import { useSetRecoilState } from 'recoil';
-import pageGuideState from '@store/pageGuide';
-
-
-const StartPageGuide: React.FC = () => {
-    const setPageGuide = useSetRecoilState(pageGuideState);
-
-
-    const closePageGuide = () => {
-        setPageGuide('');
-    };
-
-    return (
-        <PageGuide>
-            {/* <GuideContent x좌표=? y좌표=? 내용=""/> */}
-            <GuideContent />
-            <GuideContent />
-            <GuideContent />
-            <GuideContent />
-        </PageGuide>
-    )
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import guide1 from '@img/guide/StartPageGuide1.PNG';
+interface ArrowProps {
+  isLeft: boolean;
 }
 
+const images = [guide1];
+
+const Container = styled.div`
+  width: 800px;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FillImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Arrow = styled.div<ArrowProps>`
+  display: flex;
+  width: 50px;
+  height: 50px;
+  background-color: gray;
+  border-radius: 50%;
+  ${(props) => (props.isLeft ? 'left: 5px' : 'right: 5px')};
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+`;
+
+const StartPageGuide: React.FC = () => {
+  const [guideIndex, setGuideIndex] = useState<number>(0);
+
+  const handlePrevClick = useCallback((): void => {
+    if (guideIndex <= 0) {
+        setGuideIndex(images.length - 1);
+      return;
+    }
+    setGuideIndex(guideIndex - 1);
+  }, [guideIndex]);
+
+  const handleNextClick = useCallback((): void => {
+    if (guideIndex + 1 === images.length) {
+        setGuideIndex(0);
+      return;
+    }
+    setGuideIndex(guideIndex + 1);
+  }, [guideIndex]);
+
+  return (
+    <Container>
+      <Arrow isLeft={true} onClick={handlePrevClick} id="left_arrow">
+        <AiOutlineArrowLeft />
+      </Arrow>
+
+      <FillImage src={images[guideIndex]} />
+
+      <Arrow isLeft={false} onClick={handleNextClick} id="right_arrow">
+        <AiOutlineArrowRight />
+      </Arrow>
+    </Container>
+    // <Test>테스트</Test>
+  );
+};
 
 export default StartPageGuide;
---
+++ client/src/components/molecules/StartPageGuideModal.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import Modal from '@atoms/Modal';
+import { useSetRecoilState } from 'recoil';
+import modalState from '@store/modal';
+import StartPageGuide from './StartPageGuide';
+
+const Content = styled.div`
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`
+const CloseButton = styled.div`
+  display: flex;
+  width: 50px;
+  height: 50px;
+  background-color: gray;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: white;
+`;
+
+const StartPageGuideModal: React.FC = () => {
+
+  const setModal = useSetRecoilState(modalState);
+
+  const closeModal = () => {
+    setModal('');
+  };
+
+  return (
+    <Modal>
+      <Content>
+        <ButtonContainer>
+          <CloseButton onClick={closeModal}>X</CloseButton>
+        </ButtonContainer>
+        <StartPageGuide/>
+      </Content>
+    </Modal>
+  );
+};
+
+export default StartPageGuideModal;
