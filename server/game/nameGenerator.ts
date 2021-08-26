f8ee55e8214749dd1eb95fc428653651b9c45721
JaeMyoung Suh <kevindec18@ajou.ac.kr>
2021-08-26 12:55:07 +0900

    refactor : generator refactoring

--- server/game/nameGenerator.ts
+++ server/game/nameGenerator.ts
@@ -4,29 +4,34 @@ Random Name Generator for Javascript
 */
 import { firstName, lastName } from './nameList';
 
-const {users}=require('../socket/users');
+const { users } = require('../socket/users');
 
-function capFirst(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getRandomInt(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function checkDuplicate(name:string){
-  if(!users) return true;
-  const names = users.map((user:any)=>user.name);
-  if(names.find((existName:string)=>existName===name)===-1) return true;
+const checkDuplicate = (name: string) => {
+  if (!users) return true;
+  const names = users.map((user: any) => user.name);
+  if (names.find((existName: string) => existName === name) === -1) return true;
   else return false;
-}
-function generateName() {
-  while(true){
-    let name: string =
-      capFirst(firstName[getRandomInt(0, firstName.length + 1)]) +
-      ' ' +
-      capFirst(lastName[getRandomInt(0, lastName.length + 1)]);
-    if(checkDuplicate(name)) return name;
+};
+
+const capFirst = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const getRandomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const generateName = () => {
+  return (
+    capFirst(firstName[getRandomInt(0, firstName.length + 1)]) +
+    ' ' +
+    capFirst(lastName[getRandomInt(0, lastName.length + 1)])
+  );
+};
+
+export const getNewName = () => {
+  while (true) {
+    const name = generateName();
+    if (checkDuplicate(name)) return name;
   }
-}
-module.exports = {generateName};
+};
