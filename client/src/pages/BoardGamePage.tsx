d2c88e22f665eaab717fbf7bebc6c9e08bcfa693
do02reen24 <do02reen24@gmail.com>
2021-09-03 22:49:13 +0900

    feat : Game 영역 분리

--- client/src/pages/BoardGamePage.tsx
+++ client/src/pages/BoardGamePage.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import AllowDiceAgain from '@organisms/AllowDiceAgain';
+import styled from 'styled-components';
 import Dice from '@organisms/Dice';
 import Board from '@organisms/Board';
+import GameInfo from '@organisms/GameInfo';
+
+const GameContainer = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const BoardContainer = styled.div`
+  flex: 1;
+`;
+
+const GameInfoContainer = styled.div`
+  width: 400px;
+  border-left: 1px solid black;
+`;
 
 const BoardGamePage: React.FC = () => {
   return (
-    <>
+    <GameContainer>
       {/* <AllowDiceAgain /> */}
       <Dice />
-      <Board />
-    </>
+      <BoardContainer>
+        <Board />
+      </BoardContainer>
+      <GameInfoContainer>
+        <GameInfo />
+      </GameInfoContainer>
+    </GameContainer>
   );
 };
 
