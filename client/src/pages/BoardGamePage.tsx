8af73dbc007e6efa7ddea51aecf8f7663f0cdf4d
dmstmdrbs <dmstmdrbs98@gmail.com>
2021-09-04 20:07:32 +0900

    feat : select game order before board shown

--- client/src/pages/BoardGamePage.tsx
+++ client/src/pages/BoardGamePage.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AllowDiceAgain from '@organisms/AllowDiceAgain';
 import Board from '@organisms/Board';
+import SelectGameOrder from '@organisms/SelectGameOrder';
+import styled from 'styled-components';
 
+import { useRecoilState } from 'recoil';
+import userState from '@store/user';
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 const BoardGamePage: React.FC = () => {
+  const [user, setUser] = useRecoilState(userState);
+  const [startCount, setStartCount] = useState(5);
+  const [isGame, setIsGame] = useState(false);
+  let startTimer;
+  useEffect(() => {
+    if (user.isGame) {
+      gameStart();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (startCount === 0) {
+      setIsGame(true);
+    }
+  }, [startCount]);
+
+  const gameStart = () => {
+    startTimer = setInterval(() => {
+      setStartCount((prev) => prev - 1);
+      if (startCount < 0) {
+        clearInterval(startTimer);
+      }
+    }, 1000);
+  };
   return (
-    <>
-      <p>보드게임 페이지입니다.</p>
+    <Container>
+      {!user.isGame && <SelectGameOrder />}
       {/* <AllowDiceAgain /> */}
-      <Board />
-    </>
+
+      {user.isGame && isGame ? (
+        <Board />
+      ) : user.isGame ? (
+        <p>곧 게임이 시작됩니다! {startCount}초 전</p>
+      ) : null}
+    </Container>
   );
 };
 
