5adc9915b4aa6cc4ecd0fdf62b6ac2268fffb1d1
Coreight98 <maxcha98@naver.com>
2023-05-21 22:35:02 +0900

    chore: color prettier 수정

--- refactor-client/src/theme/color.ts
+++ refactor-client/src/theme/color.ts
@@ -8,27 +8,27 @@ interface Colors {
 
 const color: Colors = {
   primary: {
-    darkYellow: '#FDBE1F',
-    lightBrown: '#DCA660',
-    gray: '#d3d3d3',
-    black: '#4a4a4a',
-    blue: '#64b2ff',
+    darkYellow: "#FDBE1F",
+    lightBrown: "#DCA660",
+    gray: "#d3d3d3",
+    black: "#4a4a4a",
+    blue: "#64b2ff",
   },
   bakery: {
-    doorBack: '#DDA660',
+    doorBack: "#DDA660",
   },
   background: {
-    main: '#f6f6d3',
-    translucentBlack: '#00000050',
+    main: "#f6f6d3",
+    translucentBlack: "#00000050",
   },
   button: {
-    darkYellow: '#FDBE1F',
-    yellow: '#f9f4c6',
-    lightYellow: '#f5efba',
-    orange: '#FF8E1D',
-    darkGray: '#666666',
-    gray: '#d3d3d3',
-    lightGray: '#d8d8d8',
+    darkYellow: "#FDBE1F",
+    yellow: "#f9f4c6",
+    lightYellow: "#f5efba",
+    orange: "#FF8E1D",
+    darkGray: "#666666",
+    gray: "#d3d3d3",
+    lightGray: "#d8d8d8",
   },
 };
 
