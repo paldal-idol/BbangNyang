4c7b4764fe26880e38cefd88f99d04e5727e529d
Coreight98 <maxcha98@naver.com>
2023-05-29 18:57:04 +0900

    feat: block, board 로직 추가

--- refactor-server/src/sockets/domain/block.ts
+++ refactor-server/src/sockets/domain/block.ts
@@ -1,8 +1,25 @@
+import Piece from './piece';
+
 class Block {
-  pipe: any[];
+  pieces: Piece[];
   idx: number;
   constructor(idx) {
+    this.pieces = [];
     this.idx = idx;
+  }
+  popPiece() {
+    return this.pieces.pop();
+  }
+  popPieces() {
+    const pieces = this.pieces;
+    this.pieces = [];
+    return pieces;
+  }
+  pushPiece(piece) {
+    this.pieces.push(piece);
+  }
+  pushPieces(pieces) {
+    this.pieces.concat(pieces);
   }
 }
 
--- refactor-server/src/sockets/domain/board.ts
+++ refactor-server/src/sockets/domain/board.ts
@@ -2,15 +2,48 @@ import Block from './block';
 
 class Board {
   maxBlock: number;
-  blockList;
+  blocks: Block[];
 
-  constructor(pieceList) {
+  constructor(pieces) {
     this.maxBlock = 21;
-    this.blockList = Array.from({ length: this.maxBlock }).map((_, idx) => new Block(idx));
-    this.blockList[0] = pieceList;
+    this.blocks = Array.from({ length: this.maxBlock }).map((_, idx) => new Block(idx));
+    this.blocks[0] = pieces;
   }
 
-  changePiecePosition(prevPosition, nextPosition) {}
+  changePiecePosition(prevPosition, nextPosition) {
+    const blockLength = this.blocks[prevPosition].pieces.length;
+
+    if (blockLength === 1) {
+      this.replacePiece(prevPosition, nextPosition);
+    } else if (blockLength > 1) {
+      this.replacePieces(prevPosition, nextPosition);
+    } else {
+      throw new Error('빈 공간입니다.');
+    }
+  }
+
+  replacePiece(prevPosition, nextPosition) {
+    this.blocks[nextPosition].pushPiece(this.blocks[prevPosition].popPiece());
+  }
+  replacePieces(prevPosition, nextPosition) {
+    this.blocks[nextPosition].pushPieces(this.blocks[prevPosition].popPieces());
+  }
+
+  compareCircuits(position) {
+    return position > this.maxBlock;
+  }
+
+  calculatePosition(position) {
+    return position % this.maxBlock;
+  }
+
+  move(prevPosition, nextPosition) {
+    if (this.compareCircuits(nextPosition)) {
+      this.blocks[prevPosition].pieces[0].user.score += 1;
+    }
+    const position = this.calculatePosition(nextPosition);
+    this.changePiecePosition(prevPosition, position);
+  }
 }
 
 export default Board;
