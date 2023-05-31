c07b029ce5dabc1dc0c5b0d7eeb7f0187b37c183
Coreight98 <maxcha98@naver.com>
2023-05-31 17:15:50 +0900

    feat: 포인트 생성 로직 추가

--- refactor-server/src/sockets/services/game/index.ts
+++ refactor-server/src/sockets/services/game/index.ts
@@ -2,3 +2,10 @@ export const reArrange = <T>(arr: T[]) => {
   const firstEl = arr[0];
   return [...arr.slice(1), firstEl];
 };
+
+const MAX_POINT = 6;
+const MIN_POINT = 1;
+
+export const generateRandomNumbers = (count) => {
+  return Array.from({ length: count }, () => Math.floor(Math.random() * MAX_POINT) + MIN_POINT);
+};
