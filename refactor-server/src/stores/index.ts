866502c6e19153d619dd09d610a9d83bc3f9be30
Coreight98 <maxcha98@naver.com>
2023-06-01 23:51:47 +0900

    feat: 게임 스토어 추가

--- refactor-server/src/stores/index.ts
+++ refactor-server/src/stores/index.ts
@@ -1,7 +1,14 @@
+import Board from '../sockets/domain/board';
 import Room from '../sockets/domain/room';
 
 export type RoomsType = {
   [code: string]: Room;
 };
 
+export type GameType = {
+  [code: string]: Board;
+};
+
 export let rooms: RoomsType = {};
+
+export let games: GameType = {};
